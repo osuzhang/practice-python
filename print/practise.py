5938fe3c7f92f9f69c4736aaa0cc437e679dbceb
osuzhang <osuzhang@gmail.com>
2023-12-10 16:32:07 -0500

    add test.py

--- print/practise.py
+++ print/practise.py
@@ -1,4 +1,16 @@
+
+
+# asdf
+# asdfsadf
+# asdf
+# asdf
+# asdf
 print(ord('北'))
 print(ord('京'))
 print(chr(21271))
 print(chr(20214))
+fp = open('test.txt', 'w')
+fp.write('hello')
+# print("welcome to beijing",file=fp)
+print("北京华英你", file=fp)
+fp.close()
